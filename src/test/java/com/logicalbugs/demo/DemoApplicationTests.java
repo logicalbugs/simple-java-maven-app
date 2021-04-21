db3381100aa96f1044b348e841322bae9ee00c72
Manoj Kumar <ManojKumar@fico.com>
2021-04-22 01:40:36 +0530

    fix tests

--- src/test/java/com/logicalbugs/demo/DemoApplicationTests.java
+++ src/test/java/com/logicalbugs/demo/DemoApplicationTests.java
@@ -2,17 +2,45 @@ package com.logicalbugs.demo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.IOException;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-@SpringBootTest(classes = DemoApplication.class)
-public class DemoApplicationTests extends AbstractTest {
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@WebMvcTest(DemoApplication.class)
+public class DemoApplicationTests {
+	@Autowired
+	protected MockMvc mvc;
+
+	@BeforeEach
+	protected void setUp() {
+	}
+
+	protected String mapToJson(Object obj) throws JsonProcessingException {
+		ObjectMapper objectMapper = new ObjectMapper();
+		return objectMapper.writeValueAsString(obj);
+	}
+
+	protected <T> T mapFromJson(String json, Class<T> clazz)
+			throws JsonParseException, JsonMappingException, IOException {
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		return objectMapper.readValue(json, clazz);
+	}
 
 	@Test
 	void contextLoads() {
-		
+
 	}
 
 	@Test
